475e077efc34e0729a42e3a6cb4b63c865ea2288
Shannon Kuehn <shannon.kuehn@gmail.com>
2018-12-11 10:27:22 -0600

    Update UM-ServerGrp&Sched.ps1

--- PowerShell/PoSH-Preview/UM-ServerGrp&Sched.ps1
+++ PowerShell/PoSH-Preview/UM-ServerGrp&Sched.ps1
@@ -13,8 +13,44 @@ involved in the creation, production, or delivery of the scripts be liable for a
 damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the 
 use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages.
 #>
+#
+# Flow of PowerShell Script
+# Part 1:
+# Groups servers based upon a CSV file input. The CSV file contains a patch schedule code. Using the patch schedule code, 
+# along with a Heartbeat query to get all servers checked into Log Analytics for Update Management, the script compares
+# both data sources and groups servers into variables for patch scheduling.
 
+$scriptBlock = .{
+$serverinfo = Get-Content -Path "C:\Users\skuehn\Documents\servergrouping.csv" | ConvertFrom-Csv
+$query = "Heartbeat | summarize arg_max(TimeGenerated, *) by SourceComputerId | top 500000 by Computer asc"
+$queryResults = Invoke-AzureRmOperationalInsightsQuery -WorkspaceId "511b3784-4652-45d3-a744-53a6dc023326" -Query $query
+$queryResults.Results
+$servers = $queryResults.Results | Select-Object -ExpandProperty Computer
+}
+$group1 = @()
+$group2 = @()
+$group3 = @()
 
+ForEach($server in $serverinfo) 
+{
+        Switch ($server.patchSchedule){
+            'patch_schedule01' {
+                $new1 = $servers | ? {$_ -eq $server.servername} | Select-Object -First 1
+                $group1+=$new1
+            }
+            'patch_schedule02' {
+                $new2 = $servers | ? {$_ -eq $server.servername} | Select-Object -First 1
+                $group2+=$new2
+            }
+            'patch_schedule03' {
+                $new3 = $servers | ? {$_ -eq $server.servername} | Select-Object -First 1
+                $group3+=$new3
+            }
+        }
+}
+
+# Part 2:
+# Takes variables from groups created and places them into a patch schedule.
 function SetSchedule {
 param(
 [Parameter(Mandatory=$true)]
@@ -54,33 +90,4 @@ $schedule = New-AzureRmAutomationSchedule -ResourceGroupName $resourceGroup `
                                                      -NonAzureComputer $group `
                                                      -IncludedUpdateClassification Critical `
                                                      -Duration $duration
-}
-
-$scriptBlock = .{
-$serverinfo = Get-Content -Path "C:\Users\skuehn\Documents\servergrouping.csv" | ConvertFrom-Csv
-$query = "Heartbeat | summarize arg_max(TimeGenerated, *) by SourceComputerId | top 500000 by Computer asc"
-$queryResults = Invoke-AzureRmOperationalInsightsQuery -WorkspaceId "511b3784-4652-45d3-a744-53a6dc023326" -Query $query
-$queryResults.Results
-$servers = $queryResults.Results | Select-Object -ExpandProperty Computer
-}
-$group1 = @()
-$group2 = @()
-$group3 = @()
-
-ForEach($server in $serverinfo) 
-{
-        Switch ($server.patchSchedule){
-            'patch_schedule01' {
-                $new1 = $servers | ? {$_ -eq $server.servername} | Select-Object -First 1
-                $group1+=$new1
-            }
-            'patch_schedule02' {
-                $new2 = $servers | ? {$_ -eq $server.servername} | Select-Object -First 1
-                $group2+=$new2
-            }
-            'patch_schedule03' {
-                $new3 = $servers | ? {$_ -eq $server.servername} | Select-Object -First 1
-                $group3+=$new3
-            }
-        }
 }
