2e06e8280345d4f253b5df2cc522aa3ad43a8369
Shannon Kuehn <shannon.kuehn@gmail.com>
2019-02-27 15:40:44 -0600

    Create linRollBkPatch.py

---
+++ PowerShell/AzureAutomationRunbooks/RollbackPatches/linRollBkPatch.py
@@ -0,0 +1,48 @@
+import os
+from azure.mgmt.compute import ComputeManagementClient
+import azure.mgmt.resource
+import automationassets
+
+def get_automation_runas_credential(runas_connection):
+    from OpenSSL import crypto
+    import binascii
+    from msrestazure import azure_active_directory
+    import adal
+
+    # Get the Azure Automation RunAs service principal certificate
+    cert = automationassets.get_automation_certificate("AzureRunAsCertificate")
+    pks12_cert = crypto.load_pkcs12(cert)
+    pem_pkey = crypto.dump_privatekey(crypto.FILETYPE_PEM,pks12_cert.get_privatekey())
+
+    # Get run as connection information for the Azure Automation service principal
+    application_id = runas_connection["ApplicationId"]
+    thumbprint = runas_connection["CertificateThumbprint"]
+    tenant_id = runas_connection["TenantId"]
+
+    # Authenticate with service principal certificate
+    resource ="https://management.core.windows.net/"
+    authority_url = ("https://login.microsoftonline.com/"+tenant_id)
+    context = adal.AuthenticationContext(authority_url)
+    return azure_active_directory.AdalAuthentication(
+    lambda: context.acquire_token_with_client_certificate(
+            resource,
+            application_id,
+            pem_pkey,
+            thumbprint)
+    )
+
+# Authenticate to Azure using the Azure Automation RunAs service principal
+runas_connection = automationassets.get_automation_connection("AzureRunAsConnection")
+azure_credential = get_automation_runas_credential(runas_connection)
+
+# Initialize the compute management client with the RunAs credential and specify the subscription to work against.
+compute_client = ComputeManagementClient(
+azure_credential,
+  str(runas_connection["SubscriptionId"])
+)
+
+list_of_servers = ["host1", "host2", "host3", "host4", "host5"]
+
+for server in list_of_servers:
+  import subprocess
+  subprocess.call(['yum remove package_name'], shell=True)
