d9458ac06ba33ead05e58fd5b41e7d140d7720d4
Shannon Kuehn <shannon.kuehn@gmail.com>
2019-08-16 20:50:58 -0500

    Update 01-BearerToken.ps1

--- PowerShell/AutomatedComplianceReporting/01-BearerToken.ps1
+++ PowerShell/AutomatedComplianceReporting/01-BearerToken.ps1
@@ -16,31 +16,27 @@ documentation, even if Microsoft has been advised of the possibility of such dam
 # Adapted from: 
 # https://www.codeisahighway.com/how-to-easily-and-silently-obtain-accesstoken-bearer-from-an-existing-azure-powershell-session/
 
-function Get-AzureRmCachedAccessToken()
+function Get-AzCachedAccessToken()
 {
-  $ErrorActionPreference = 'Stop'
+    $ErrorActionPreference = 'Stop'
   
-  if(-not (Get-Module AzureRm.Profile)) {
-    Import-Module AzureRm.Profile
-  }
-  $azureRmProfileModuleVersion = (Get-Module AzureRm.Profile).Version
-  # refactoring performed in AzureRm.Profile v3.0 or later
-  if($azureRmProfileModuleVersion.Major -ge 3) {
-    $azureRmProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
-    if(-not $azureRmProfile.Accounts.Count) {
-      Write-Error "Ensure you have logged in before calling this function."    
+    if(-not (Get-Module Az.Accounts)) {
+        Import-Module Az.Accounts
     }
-  } else {
-    # AzureRm.Profile < v3.0
-    $azureRmProfile = [Microsoft.WindowsAzure.Commands.Common.AzureRmProfileProvider]::Instance.Profile
-    if(-not $azureRmProfile.Context.Account.Count) {
-      Write-Error "Ensure you have logged in before calling this function."    
+    $azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+    if(-not $azProfile.Accounts.Count) {
+        Write-Error "Ensure you have logged in before calling this function."    
     }
-  }
   
-  $currentAzureContext = Get-AzureRmContext
-  $profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azureRmProfile)
-  Write-Debug ("Getting access token for tenant" + $currentAzureContext.Subscription.TenantId)
-  $token = $profileClient.AcquireAccessToken($currentAzureContext.Subscription.TenantId)
-  $token.AccessToken
+    $currentAzureContext = Get-AzContext
+    $profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azProfile)
+    Write-Debug ("Getting access token for tenant" + $currentAzureContext.Tenant.TenantId)
+    $token = $profileClient.AcquireAccessToken($currentAzureContext.Tenant.TenantId)
+    $token.AccessToken
+}
+
+function Get-AzBearerToken()
+{
+    $ErrorActionPreference = 'Stop'
+    ('Bearer {0}' -f (Get-AzCachedAccessToken))
 }
